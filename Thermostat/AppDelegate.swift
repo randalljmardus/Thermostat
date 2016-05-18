39b68f362249be8086d3f045cdad9d676c947ff9
Randall Mardus <randalljmardus@gmail.com>
2016-05-18 11:30:25 -0400

    Initial Commit

---
+++ Thermostat/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Thermostat
+//
+//  Created by Randall Mardus on 5/18/16.
+//  Copyright © 2016 Randall Mardus. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
