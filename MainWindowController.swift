0c42a8ab8e34174d963bca6bb8105276ff10f322
Randall Mardus <randalljmardus@gmail.com>
2016-05-18 11:40:20 -0400

    initial commit

---
+++ MainWindowController.swift
@@ -0,0 +1,23 @@
+//
+//  MainWindowController.swift
+//  Thermostat
+//
+//  Created by Randall Mardus on 5/18/16.
+//  Copyright © 2016 Randall Mardus. All rights reserved.
+//
+
+import Cocoa
+
+class MainWindowController: NSWindowController {
+
+    override var windowNibName: String {
+        return "MainWindowController"
+    }
+    
+    override func windowDidLoad() {
+        super.windowDidLoad()
+
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+    
+}
--- Thermostat/AppDelegate.swift
+++ Thermostat/AppDelegate.swift
@@ -11,16 +11,19 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    @IBOutlet weak var window: NSWindow!
-
-
+    var mainWindowController: MainWindowController?
+    
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        //Create a window controller
+        let mainWindowController = MainWindowController()
+        
+        //Put the window of the window controller on screen
+        mainWindowController.showWindow(self)
+        
+        //Set the property to point to the window controller
+        self.mainWindowController = mainWindowController
     }
 
-    func applicationWillTerminate(aNotification: NSNotification) {
-        // Insert code here to tear down your application
-    }
 
 
 }
