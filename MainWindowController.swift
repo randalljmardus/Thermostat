5dad65b1abb69c5f74c7fb428ff3a79e1882ef1a
Randall Mardus <randalljmardus@gmail.com>
2016-05-18 12:20:14 -0400

    made keys observable and dynamic

--- MainWindowController.swift
+++ MainWindowController.swift
@@ -9,15 +9,25 @@
 import Cocoa
 
 class MainWindowController: NSWindowController {
-
+    
     override var windowNibName: String {
         return "MainWindowController"
     }
+    
+    dynamic var temperature = 68
     
     override func windowDidLoad() {
         super.windowDidLoad()
 
         // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+    
+    @IBAction func makeWarmer(sender: NSButton) {
+        temperature++
+    }
+    
+    @IBAction func makeCooler(sender: NSButton) {
+        temperature--
     }
     
 }
